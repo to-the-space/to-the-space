fd49b317a904090a21faff38d1b6e233721b978d
Seo Jun Yoo <sytwoyou@gmail.com>
2021-10-12 10:45:08 +0900

    Feat: arrow key control for spaceship

--- src/game/models/Spaceship.js
+++ src/game/models/Spaceship.js
@@ -1,10 +1,13 @@
 import * as CANNON from "cannon-es";
 import { autorun } from "mobx";
 
+import { STATE } from "../../constants/view";
+
 import Model from "./Model";
 import Controls from "../Controls";
 
 import playStore from "../../store/playStore";
+import viewStore from "../../store/viewStore";
 
 class Spaceship extends Model {
   constructor(model, scene, physicsWorld) {
@@ -25,17 +28,22 @@ class Spaceship extends Model {
       return;
     }
     if (this.input.keys.left) {
-      this.model.position.x -= 0.4;
+      this.boxBody.position.x -= 0.4;
+      // this.boxBody.quaternion.y -= 0.01;
     }
     if (this.input.keys.right) {
-      this.model.position.x += 0.4;
+      this.boxBody.position.x += 0.4;
+      // this.boxBody.quaternion.y += 0.01;
     }
   }
 
   launch() {
     const power = playStore.power;
 
-    this.boxBody.applyForce(new CANNON.Vec3(0, power, 0), this.boxBody.position);
+    this.boxBody.applyForce(
+      new CANNON.Vec3(0, power, 0),
+      this.boxBody.position,
+    );
 
     this.boxBody.addEventListener("collide", () => {});
   }
@@ -44,8 +52,21 @@ class Spaceship extends Model {
     this.model.position.copy(this.boxBody.position);
     this.model.quaternion.copy(this.boxBody.quaternion);
 
+    const speed = Math.floor(this.boxBody.velocity.y);
+    if (speed > -1) {
+      this.enableControl();
+      playStore.changeSpeed(speed);
+    }
+
     const altitude = Math.floor(this.model.position.y);
-    playStore.changeAltitude(altitude);
+    if (altitude > -1) {
+      playStore.changeAltitude(altitude);
+      playStore.setHighestAltitude(altitude);
+    }
+
+    if (speed < 0) {
+      viewStore.updateState(STATE.END);
+    }
   }
 }
 
