0c1f28cbce84db4bc70d9f46a34f45582e63e7a6
Seo Jun Yoo <sytwoyou@gmail.com>
2021-10-12 10:45:41 +0900

    Chore: modify webpack configuration

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -5,7 +5,9 @@ const portFinderSync = require("portfinder-sync");
 module.exports = merge(commonConfiguration, {
   mode: "development",
   devServer: {
+    static: { directory: path.join(__dirname, "./dist") },
     compress: true,
+    hot: true,
     port: portFinderSync.getPort(3000),
   },
 });
