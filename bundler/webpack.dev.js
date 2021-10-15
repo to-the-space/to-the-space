9ed37ea1c5251f187b7f493db56bcc0a86d9d764
Seo Jun Yoo <sytwoyou@gmail.com>
2021-10-15 11:07:15 +0900

    Chore: webpack configuration optimization

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const commonConfiguration = require("./webpack.common.js");
 const portFinderSync = require("portfinder-sync");
+const path = require("path");
 
 module.exports = merge(commonConfiguration, {
   mode: "development",
--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,11 +1,30 @@
 const { merge } = require("webpack-merge");
+const TerserPlugin = require("terser-webpack-plugin");
 const commonConfiguration = require("./webpack.common.js");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = merge(commonConfiguration, {
   mode: "production",
-  devtool: false,
+  devtool: "eval-cheap-module-source-map",
+  performance: {
+    hints: false,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+  },
   plugins: [
+    new CleanWebpackPlugin(),
     new Dotenv({
       systemvars: true,
     }),
